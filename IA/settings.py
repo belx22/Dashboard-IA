7fc6668d597635d2c5c26b7b5d8caa12638e2d4a
belx22 <bellofidele@gmail.com>
2021-06-12 00:29:00 +0100

    12/06/2021 00-28

--- IA/settings.py
+++ IA/settings.py
@@ -78,10 +78,8 @@ WSGI_APPLICATION = 'IA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'ia',
-        'HOST':'localhost',
-        'USER':'root',
+         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
    
      
     }
